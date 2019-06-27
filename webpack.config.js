4a8849506cefdf708634146481caad1ac42da314
aruyamam <aruyamam@gmail.com>
2019-06-27 16:26:54 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+   entry: ['./src/js/index.js', './src/styles/style.scss'],
+   output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'js/bundle.js',
+   },
+   devServer: {
+      contentBase: './dist',
+   },
+   plugins: [
+      new HtmlWebpackPlugin({
+         filename: 'index.html',
+         template: './src/index.html',
+      }),
+      new MiniCSSExtractPlugin({
+         filename: 'css/style.css',
+      }),
+   ],
+   devtool: 'source-map',
+   module: {
+      rules: [
+         {
+            test: /\.s?css$/,
+            use: [
+               MiniCSSExtractPlugin.loader,
+               {
+                  loader: 'css-loader',
+                  options: {
+                     sourceMap: true,
+                  },
+               },
+               {
+                  loader: 'postcss-loader',
+                  options: {
+                     sourceMap: true,
+                     plugins: [require('autoprefixer')({ grid: true })],
+                  },
+               },
+               {
+                  loader: 'sass-loader',
+                  options: {
+                     sourceMap: true,
+                  },
+               },
+            ],
+         },
+         {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+               loader: 'babel-loader',
+            },
+         },
+         {
+            test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+            use: ['file-loader'],
+         },
+      ],
+   },
+};
